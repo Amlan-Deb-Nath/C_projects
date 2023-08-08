f171ebd2d72eb2b497918de70ffb9af4e0e66ec9
Amlan <141430102+Amlan-Deb-Nath@users.noreply.github.com>
2023-08-08 20:01:58 +0600

    Add files via upload
    all c projects are included here

---
+++ largest_number_out_of_3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter the fist number :\n");
+    scanf("%d",&a);
+
+    printf("Enter the second number :\n");
+    scanf("%d",&b);
+
+    printf("Enter the third number :\n");
+    scanf("%d",&c);
+
+    if (a>b && a>c){
+        printf("The largest number among the three is : %d ",a);
+    }
+    else if (b>a && b>c){
+         printf("The largest number among the three is : %d ",b);
+    }
+    else
+     printf("The largest number among the three is : %d ",c);
+
+     
+     getch();
+
+}
